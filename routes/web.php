361626bec3b99c89dc0c6e2494c4c2b43e129610
Sandesh Pahari <sandeshpahari05@gmail.com>
2025-08-28 14:57:24 +0545

    esewa routes created

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EsewaController;
 
 Route::get('/', function () {
     return view('template');
 });
+
+
+Route::get('/pay/{amount}', [EsewaController::class, 'initiate'])->name('esewa.pay');
+Route::post('/esewa/success', [EsewaController::class, 'success'])->name('esewa.success');
+Route::post('/esewa/failure', [EsewaController::class, 'failure'])->name('esewa.failure');
